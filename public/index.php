72bd5fb3316d15e419b3a55a79dd57f58ee6e8b8
Zuam Xness <apkssky@gmail.com>
2018-11-09 16:31:36 +0700

    Update index

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin:*');
-header('Access-Control-Allow-Methods:GET,POST');
+header('Access-Control-Allow-Methods:GET,POST,PATCH,DELETE');
 
 /**
  * Laravel - A PHP Framework For Web Artisans
